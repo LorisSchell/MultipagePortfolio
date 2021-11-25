f25aedbfe89d7f3fd8d164165d1568cd0a63105a
LorisSchell <betgov-soszi9-dymGun>
2021-11-25 13:16:05 +0100

    Initialization of project

---
+++ public/js/formular.js
@@ -0,0 +1,8 @@
+function pressed() {
+  console.log("btn1 gedrückt");
+}
+
+var btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", function () {
+  console.log("btn2 gedrückt");
+});
