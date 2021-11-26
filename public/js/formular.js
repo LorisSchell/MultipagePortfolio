7fc9313ba60781a01c0bc8af45068e6dadf233c1
LorisSchell <schell.loris@t-online.de>
2021-11-26 15:32:37 +0100

    Homepage basic design

--- public/js/formular.js
+++ public/js/formular.js
@@ -3,6 +3,6 @@ function pressed() {
 }
 
 var btn2 = document.getElementById("btn2");
-btn2.addEventListener("", function () {
+btn2.addEventListener<"click">("click", function () {
   console.log("btn2 gedrückt");
 });
